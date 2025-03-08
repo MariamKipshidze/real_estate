760b62d18b62cf3217f1538c52eabd7ad5c89e2b
Mariam <mari.kifshidze@gmail.com>
2025-03-08 21:26:40 +0400

    Register property in django admin

--- real_estate/application/admin.py
+++ real_estate/application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Property, PropertyImage
 
-# Register your models here.
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 1  # Number of extra forms to show for image uploads
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'property_type', 'location', 'created_at', 'updated_at']
+    search_fields = ['title', 'location', 'price']
+    list_filter = ['property_type']
+    inlines = [PropertyImageInline]
+
+admin.site.register(Property, PropertyAdmin)

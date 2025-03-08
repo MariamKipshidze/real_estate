e546c46b4f2fe49028cccf8a23b75bf41732a2b7
Mariam <mari.kifshidze@gmail.com>
2025-03-08 21:14:22 +0400

    override user model

--- real_estate/user/admin.py
+++ real_estate/user/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user.models import CustomUser  # Import your CustomUser model
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'first_name', 'last_name', 'phone_number', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['email']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+            'email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff'),
+        }),
+    )
+
+
+# Register the CustomUser model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)

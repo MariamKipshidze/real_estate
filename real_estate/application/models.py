4e0e5883f150de3c937a6f1e97182ec502af33da
Mariam <mari.kifshidze@gmail.com>
2025-03-09 12:25:34 +0400

    Add agent field to Property model

--- real_estate/application/models.py
+++ real_estate/application/models.py
@@ -10,21 +10,25 @@ class Property(models.Model):
         ('commercial', 'Commercial'),
     ]
 
-    # Property Fields
+    agent = models.ForeignKey(
+        to="user.CustomUser",
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=12, decimal_places=2)  # Price of the property
+    price = models.DecimalField(max_digits=12, decimal_places=2)
     property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES, default='house')  # Type of the property
-    location = models.CharField(max_length=255)  # Location (address) of the property
-    created_at = models.DateTimeField(auto_now_add=True)  # Date the property was created
-    updated_at = models.DateTimeField(auto_now=True)  # Date the property was last updated
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
 
 class PropertyImage(models.Model):
-    # Image field for storing property images
     property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='property_images/')
     description = models.CharField(max_length=255, blank=True, null=True)
